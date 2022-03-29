bdfc0804da23db28fa3ea0fd1a2856f706564e52
Colton Watkins <watkincw@gmail.com>
2022-03-29 12:11:40 -0500

    Setup 'index.js' to use 'redux-devtools-extension' and 'App' instead of the 'Posts' component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 // import counterReducer from './reducer/CounterReducer';
-// import ColorReducer from './reducer/ColorReducer';
-import postReducer from './reducer/PostReducer';
+import ColorReducer from './reducer/ColorReducer';
+// import postReducer from './reducer/PostReducer';
 import { Provider } from 'react-redux';
-import Posts from './components/Posts';
+// import Posts from './components/Posts';
 // import logger from 'redux-logger';
-// import { myLogger } from './middleware/myLogger';
-// import { myLogger2 } from './middleware/myLogger2';
-import thunk from 'redux-thunk';
+import { myLogger } from './middleware/myLogger';
+import { myLogger2 } from './middleware/myLogger2';
+// import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reportWebVitals from './reportWebVitals';
 
-// const store = createStore(ColorReducer, applyMiddleware(myLogger, myLogger2, logger));
-const store = createStore(postReducer, applyMiddleware(thunk));
+const devTools = composeWithDevTools(applyMiddleware(myLogger, myLogger2));
+const store = createStore(ColorReducer, devTools);
+// const store = createStore(postReducer, applyMiddleware(thunk));
 console.log('Store Created!');
 
 ReactDOM.render(
 	<Provider store={store}>
-		<Posts />
-		{/* <App /> */}
+		{/* <Posts /> */}
+		<App />
 	</Provider>,
 	document.getElementById('root')
 );
