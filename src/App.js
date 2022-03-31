8f0d8aa00d70e66e5e1e2e7f9292eb6d17f07051
Colton Watkins <watkincw@gmail.com>
2022-03-31 17:33:33 -0500

    more test practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { render, screen } from '@testing-library/react';
 import User from './components/User';
 import './App.css';
 // import { connect } from 'react-redux';
@@ -6,94 +7,112 @@ import './App.css';
 
 // import { increment, decrement } from './action/CounterAction';
 
-window.fetch = jest.fn(() => {
-	const user = {
-		name: 'Jack',
-		email: 'jack@abc.com'
-	};
-	// return Promise.resolve(user);
-	return Promise.resolve({
-		json: () => Promise.resolve(user),
-	})
-});
 
 function App(props) {
-	// // const counter = useSelector(state => state);
-	// // const dispatch = useDispatch();
+	const [counter, setCounter] = useState(0);
 
-	// const dispatch = useDispatch();
-	// const colorVal = useSelector(state => state.color);
-	// // console.log(state.color);
-
-	// // useEffect(() => {
-	// // 	console.log(props);
-	// // });
-
-	// // // This was here to help understand why the page was not being updated on select of new colors
-	// // console.log('Outside of handle change: ', colorVal);
-
-	// const handleChange = (e) => {
-	// 	if (e.target.checked) {
-	// 		// 'ADD_COLOR'
-	// 		dispatch({
-	// 			type: 'ADD_COLOR',
-	// 			payload: e.target.value
-	// 		})
-	// 	} else {
-	// 		// 'REMOVE_COLOR'
-	// 		dispatch({
-	// 			type: 'REMOVE_COLOR',
-	// 			payload: e.target.value
-	// 		})
-	// 	};
-	// }
-
-	const { user, setUser } = useState(null);
-	const [error, setError] = useState('');
-
-	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users/1')
-			.then((response) => response.json())
-			.then((user) => setUser(user))
-			.catch((error) => setError(error.message));
-	},[]);
-
-	if (error) {
-		return <span>{error}</span>
+	const increment = () => {
+		setCounter((val) => ++val);
 	}
 
+	const decrement = () => {
+		setCounter((val) => --val);
+	}
+
+
 	return (
-		<div className="App">
-			{user ? <User user={user} /> : <span>Loading...</span>}
-
-			{/* Test 1 */}
-			<h1>Hello world from React!</h1>
-			<ul className='animals'>
-				<li>Emu</li>
-				<li>Dingo</li>
-				<li>Kangaroo</li>
-				<li>Kuala</li>
-			</ul>
-
-			{/* <h1>Counter: {counter}</h1> */}
-			{/* <button onClick={() => dispatch(increment())}>increment</button> <br /> */}
-			{/* <button onClick={() => dispatch(decrement())}>decrement</button> <br /> */}
-
-			{/* <h1>Color(s): {colorVal}</h1> */}
-			{/* checkbox version */}
-			{/* <input type='checkbox' name='color' value='Red' onClick={handleChange} />Red */}
-			{/* <input type='checkbox' name='color' value='Blue' onClick={handleChange} />Blue */}
-			{/* <input type='checkbox' name='color' value='Green' onClick={handleChange} />Green */}
-			{/* <input type='checkbox' name='color' value='Purple' onClick={handleChange} />Purple */}
-			{/* <br /> */}
-			{/* <h3>Watch the console as you adjust checkbox states. The colors do not display properly, but the console displays the data correctly.</h3> */}
-			{/* radio version */}
-			{/* <input type='radio' name='color' value='Red' onClick={handleChange} />Red */}
-			{/* <input type='radio' name='color' value='Blue' onClick={handleChange} />Blue */}
-			{/* <input type='radio' name='color' value='Green' onClick={handleChange} />Green */}
-			{/* <input type='radio' name='color' value='Purple' onClick={handleChange} />Purple */}
+		<div className='App'>
+			<h1 data-testid='counter'>{counter}</h1>
+			<button onClick={increment}>increment</button>
+			<button onClick={decrement}>decrement</button>
 		</div>
-	);
+	)
+	// window.fetch = jest.fn(() => {
+	// 	const user = {
+	// 		name: 'Jack',
+	// 		email: 'jack@abc.com'
+	// 	};
+	// 	// return Promise.resolve(user);
+	// 	return Promise.resolve({
+	// 		json: () => Promise.resolve(user),
+	// 	})
+	// });
+	// // // const counter = useSelector(state => state);
+	// // // const dispatch = useDispatch();
+
+	// // const dispatch = useDispatch();
+	// // const colorVal = useSelector(state => state.color);
+	// // // console.log(state.color);
+
+	// // // useEffect(() => {
+	// // // 	console.log(props);
+	// // // });
+
+	// // // // This was here to help understand why the page was not being updated on select of new colors
+	// // // console.log('Outside of handle change: ', colorVal);
+
+	// // const handleChange = (e) => {
+	// // 	if (e.target.checked) {
+	// // 		// 'ADD_COLOR'
+	// // 		dispatch({
+	// // 			type: 'ADD_COLOR',
+	// // 			payload: e.target.value
+	// // 		})
+	// // 	} else {
+	// // 		// 'REMOVE_COLOR'
+	// // 		dispatch({
+	// // 			type: 'REMOVE_COLOR',
+	// // 			payload: e.target.value
+	// // 		})
+	// // 	};
+	// // }
+
+	// const [ user, setUser ] = useState(null);
+	// const [error, setError] = useState('');
+
+	// useEffect(() => {
+	// 	fetch('https://jsonplaceholder.typicode.com/users/1')
+	// 		.then((response) => response.json())
+	// 		.then((user) => setUser(user))
+	// 		.catch((error) => setError(error.message));
+	// },[]);
+
+	// if (error) {
+	// 	return <span>{error}</span>
+	// }
+
+	// return (
+	// 	<div className="App">
+	// 		{user ? <User user={user} /> : <span>Loading...</span>}
+
+	// 		{/* Test 1 */}
+	// 		<h1>Hello world from React!</h1>
+	// 		<ul className='animals'>
+	// 			<li>Emu</li>
+	// 			<li>Dingo</li>
+	// 			<li>Kangaroo</li>
+	// 			<li>Kuala</li>
+	// 		</ul>
+
+	// 		{/* <h1>Counter: {counter}</h1> */}
+	// 		{/* <button onClick={() => dispatch(increment())}>increment</button> <br /> */}
+	// 		{/* <button onClick={() => dispatch(decrement())}>decrement</button> <br /> */}
+
+	// 		{/* <h1>Color(s): {colorVal}</h1> */}
+	// 		{/* checkbox version */}
+	// 		{/* <input type='checkbox' name='color' value='Red' onClick={handleChange} />Red */}
+	// 		{/* <input type='checkbox' name='color' value='Blue' onClick={handleChange} />Blue */}
+	// 		{/* <input type='checkbox' name='color' value='Green' onClick={handleChange} />Green */}
+	// 		{/* <input type='checkbox' name='color' value='Purple' onClick={handleChange} />Purple */}
+	// 		{/* <br /> */}
+	// 		{/* <h3>Watch the console as you adjust checkbox states. The colors do not display properly, but the console displays the data correctly.</h3> */}
+	// 		{/* radio version */}
+	// 		{/* <input type='radio' name='color' value='Red' onClick={handleChange} />Red */}
+	// 		{/* <input type='radio' name='color' value='Blue' onClick={handleChange} />Blue */}
+	// 		{/* <input type='radio' name='color' value='Green' onClick={handleChange} />Green */}
+	// 		{/* <input type='radio' name='color' value='Purple' onClick={handleChange} />Purple */}
+	// 	</div>
+	// );
 }
 
 // // 'mapStateToProps' will subscribe to redux store changes
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from './App';
+import UserEvent from '@testing-library/user-event';
 
-// Test 1
-test('list contains 4 animals', async () => {
-	render(<App />);
-
-	const userName = await screen.findByText('Jack');
-
-	expect(userName).toBeInTheDocument()
-
-
-	// screen.debug();
-	// const listElement = screen.getByRole('list');
-	// const listItems = screen.getAllByRole('listitem');
-
-	// expect(listElement).toBeInTheDocument();
-	// expect(listElement).toHaveClass('animals');
-	// expect(listItems.length).toEqual(4);
+describe('Testing increment and decrement duttons...', () => {
+	test('Counter increment onClick...', async () => {
+		render(<App />);
+		const counter = screen.getByTestId('counter');
+		const incrementBtn = screen.getByText('increment');
+	
+		UserEvent.click(incrementBtn);
+		UserEvent.click(incrementBtn);
+	
+		expect(counter.textContent).toEqual('2');
+	});
+	
+	test('Counter decrememnt onClick...', async () => {
+		render(<App />);
+		const counter = screen.getByTestId('counter');
+		const decrementBtn = screen.getByText('decrement');
+	
+		UserEvent.click(decrementBtn);
+		UserEvent.click(decrementBtn);
+	
+		expect(counter.textContent).toEqual('-2');
+	});
 });
+
+// describe('Testing App component', () => {
+// 	// Test 1
+// 	test('list contains 4 animals', async () => {
+// 		render(<App />);
+	
+// 		const userName = await screen.findByText('Jack');
+	
+// 		expect(userName).toBeInTheDocument()
+	
+	
+// 		// screen.debug();
+// 		// const listElement = screen.getByRole('list');
+// 		// const listItems = screen.getAllByRole('listitem');
+	
+// 		// expect(listElement).toBeInTheDocument();
+// 		// expect(listElement).toHaveClass('animals');
+// 		// expect(listItems.length).toEqual(4);
+// 	});
+	
+// 	// Test 2
+// 	test('Loading text appearing...', async () => {
+// 		render(<App />);
+// 		const loadingText = screen.getByText('Loading...');
+// 		expect(loadingText).toBeInTheDocument();
+// 		await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+// 	});
+// });
